4638bfe3808ca0de6ec6db7e94df6772edf6619f
Zachary Messinger <zdmessinger@gmail.com>
2022-05-13 15:03:18 -0700

    Updated

---
+++ 7. Sorting Records/1. order_by.sql
@@ -0,0 +1,14 @@
+-- Least to Greatest
+SELECT *
+FROM products
+ORDER BY price;
+
+-- Alternative
+SELECT *
+FROM products
+ORDER BY price ASC;
+
+-- Greatest to Least
+SELECT *
+FROM products
+ORDER BY price DESC;
---
+++ 7. Sorting Records/2. order_by_charecter.sql
@@ -0,0 +1,11 @@
+-- Sort by charecters (A-Z)
+SELECT * FROM products
+ORDER BY name;
+
+SELECT * FROM products
+ORDER BY name ASC;
+
+-- Sort by charecters (Z-A)
+
+SELECT * FROM products
+ORDER BY name DESC;
