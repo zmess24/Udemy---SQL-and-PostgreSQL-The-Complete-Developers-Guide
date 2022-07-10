9f72a65d4390c847da2c07d2a271d399624bb11c
Zachary Messinger <zdmessinger@gmail.com>
2022-07-09 20:07:25 -0400

    Updated

---
+++ 0. assets/instagram_design.sql
@@ -0,0 +1,34 @@
+TABLE users {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  updated_at TIMESTAMP
+  username VARCHAR(30)
+}
+
+Table posts {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  updated_at TIMESTAMP
+  url  VARCHAR(200)
+  user_id INTEGER [ref: > users.id]
+  caption VARCHAR(240)
+  lat REAL
+  lng REAL
+}
+
+TABLE comments {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  updated_at TIMESTAMP
+  contents VARCHAR(240)
+  user_id INTEGER [ref: > users.id]
+  post_id INTEGER [ref: > posts.id]
+}
+
+Table likes {
+  id SERIAL [pk, increment]
+  created_at TIMESTAMP
+  user_id INTEGER [ref: > users.id]
+  comment_id INTEGER [ref: > comments.id]
+  post_id INTEGER [ref: > posts.id]
+}
